3a273749c3eabfd318529be0929e216967baa36e
Tural Pashayev <35586555+turalpashayev@users.noreply.github.com>
2022-04-29 16:57:34 -0700

    Update and rename intro.py to dic&iterate.py

---
+++ Python-Dicts/dic&iterate.py
@@ -0,0 +1,22 @@
+
+employees = {"Bob": 1234, "Steve": 5678, "Mike": 9012}
+
+for key, value in employees.items():
+    print(key, value)
+'''
+Output:
+Bob 1234
+Steve 5678
+Mike 9012
+''' 
+
+
+for i,j in enumareate(employees):
+    print(k,v)
+
+'''
+Output: 
+0 Bob
+1 Steve
+2 Mike
+'''
